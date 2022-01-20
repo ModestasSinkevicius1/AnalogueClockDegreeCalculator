58dd8497d778c922d8d833e1da51857fbbe58510
Modestas Sinkevicius <modestas.sinkevicius1@gmail.com>
2022-01-20 16:57:34 +0200

    End

---
+++ AnalogueClockDegreeCalculator/ClockAnalogueDegreeSolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalogueClockDegreeCalculator
+{
+    class ClockAnalogueDegreeSolver
+    {
+        private int startHour;
+        private int endHour;
+        private int startMin;
+        private int endMin;
+
+        public ClockAnalogueDegreeSolver(int startHour, int endHour, int startMin, int endMin)
+        {
+            this.startHour = startHour;
+            this.endHour = endHour;
+            this.startMin = startMin;
+            this.endMin = endMin;
+        }
+
+        /*calculates endHour and startHour to get difference between hours.
+          We know 12 hours in analogue clock creates 360 degree therefore we 
+          can use proportion trick to get degrees between hours.
+        */
+        public int CalculateHourAngle()
+        {
+            int hour = endHour - startHour;
+
+            return 360*hour/12;
+        }
+
+       /*calculates endMin and startMin to get difference between mins. 60 is 1 hour that means
+         60 min times 12 hours would give us 720 mins and that creates full loop of the clock. 
+         In the we can use proportion trick to get degrees between mins.
+       */
+        public int CalculateMinAngle()
+        {
+            int min = endMin - startMin;
+
+            return 360*min/720;
+        }
+    }
+}
--- AnalogueClockDegreeCalculator/Program.cs
+++ AnalogueClockDegreeCalculator/Program.cs
@@ -3,10 +3,33 @@
 namespace AnalogueClockDegreeCalculator
 {
     class Program
-    {
+    {       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string startHour = "0";
+            string endHour = "0";
+            
+            string startMin = "0";
+            string endMin = "0";
+
+            Console.WriteLine("Type start of hour");
+            startHour = Console.ReadLine();
+
+            Console.WriteLine("Type end of hour");
+            endHour = Console.ReadLine();
+
+            Console.WriteLine("Type start of min");
+            startMin = Console.ReadLine();
+
+            Console.WriteLine("Type end of min");
+            endMin = Console.ReadLine();
+
+            ClockAnalogueDegreeSolver cads = new ClockAnalogueDegreeSolver(Convert.ToInt32(startHour),
+                Convert.ToInt32(endHour), Convert.ToInt32(startMin), Convert.ToInt32(endMin));
+
+            Console.WriteLine($"Hour between {startHour} and {endHour} is {cads.CalculateHourAngle()}°");
+            
+            Console.WriteLine($"Min between {startMin} and {endMin} is {cads.CalculateMinAngle()}°");
         }
     }
 }
