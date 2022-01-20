18ba4dbad416da63644d1bc27c10e11e758812b5
Modestas Sinkevicius <modestas.sinkevicius1@gmail.com>
2022-01-20 13:11:58 +0200

    Add project files.

---
+++ AnalogueClockDegreeCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnalogueClockDegreeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
